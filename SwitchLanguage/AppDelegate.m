cfa91ef3dfcff13c2b054772713750623e8db6f8
ideal <lxyang6688@163.com>
2016-08-24 19:49:41 +0800

    隐藏系统返回两字
    自定义系统返回按钮，将返回按钮的文字position设置不在屏幕上显示.

--- SwitchLanguage/AppDelegate.m
+++ SwitchLanguage/AppDelegate.m
@@ -19,6 +19,13 @@
     // Override point for customization after application launch.
     [SVProgressHUD setMinimumDismissTimeInterval:2.0];
     [SVProgressHUD setDefaultAnimationType:SVProgressHUDAnimationTypeNative];
+    
+    //自定义返回按钮
+//    UIImage *backButtonImage = [[UIImage imageNamed:@""] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 30, 0, 0)];
+//    [[UIBarButtonItem appearance] setBackButtonBackgroundImage:backButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    //将返回按钮的文字position设置不在屏幕上显示
+    [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(NSIntegerMin, NSIntegerMin) forBarMetrics:UIBarMetricsDefault];
+    
     return YES;
 }
 
